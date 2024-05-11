0b47b2467f89b40de6656aa22dd60cf73f6e67e8
tonymk0987 <k.tony.muriuki@gmail.com>
2024-05-11 21:43:04 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 function App() {
   return (
-    <div></div>
-  )
+    <div>
+      <h1>Select Pizza Toppings</h1>
+      <input
+        type="checkbox"
+        id="pepperoni"
+        checked={false}
+        aria-checked={false}
+      />
+      <label htmlFor="pepperoni">Add pepperoni</label>
+    </div>
+  );
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,3 +4,17 @@ import App from "../App";
 import '@testing-library/jest-dom';
 
 // Code tests here
+// Test the initial state of the page
+
+test("pizza checkbox is initially unchecked", () => {})
+
+test("toppings list initially contains only cheese", () => {})
+test("pizza checkbox is initially unchecked", () => {
+    render(<App />);
+  
+    const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i });
+  
+    expect(addPepperoni).not.toBeChecked();
+  });
+  // Test the initial state of the page
+test("toppings list initially contains only cheese", () => {});
